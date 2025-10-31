356a90dd8471e9e8a76e02de356d67a171228331
k.pradeep <hellopradeep69@gmail.com>
2025-10-31 14:24:42 +0530

    Initial backup of i3wm configs

---
+++ i3/autostart.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Clipboard manager
+wl-paste --watch cliphist store &
+
+#copyq to have clipboard
+copyq &
+
+# Notification daemon
+dunst &
+
+# Start compositor
+picom &
+
+feh --bg-fill ~/Pictures/Wallpapers/space.jpg &
+# nitrogen --restore &
+
+# Custom scripts
+~/.local/bin/keybind.sh &
+~/.local/bin/battery-notify2.sh &
+
+# Polybar
+$HOME/.config/polybar/launch.sh &
