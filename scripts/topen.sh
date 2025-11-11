3147208e6502b4296d011f45419ff2f31de3d518
k.pradeep <hellopradeep69@gmail.com>
2025-11-11 23:35:31 +0530

    small commit

--- scripts/topen.sh
+++ scripts/topen.sh
@@ -53,7 +53,7 @@ code_start() {
 exclude_dir() {
     EXCLUDE_DIRS=(~/.tmux ~/Templates ~/.cache ~/.rustup ~/.npm ~/.zen ~/.linuxmint
         ~/Public ~/.icons ~/Desktop ~/.cargo ~/.mozilla ~/.themes ~/.w3m ~/.golf
-        ~/.java ~/.cursor ~/fastfetch ~/Telegram ~/.fzf ~/.dbus ~/Dot-conf/* ~/.pki)
+        ~/.java ~/.cursor ~/fastfetch ~/Telegram ~/.fzf ~/.dbus ~/Dot-conf/* ~/.pki ~/Music/*)
 
     exclude_args=""
     for d in "${EXCLUDE_DIRS[@]}"; do
